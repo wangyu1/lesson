47b1236b36b04ab96fe9614fbadfa671f3b65b60
wangyu1 <2391205111@qq.com>
2015-04-25 15:36:26 +0800

    update

---
+++ triangle/triangle.js
@@ -0,0 +1,15 @@
+var triangle = {
+    isEquilateral : function(a,b,c) {
+	        if(a<=0) return false ;
+	        if(a!=b) return false ;
+    	    if(b!=c) return false ;
+    		return true ;
+    		 if(a<=0) return false ;
+    },
+    isIsosceles : function(a,b,c) {
+    	if(a<=0 ) return false ; 	
+    }
+    isIsosceles : function(a,b,c) {
+    	    return true ;
+    }
+};
