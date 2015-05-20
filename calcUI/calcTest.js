acfc269345f23d71871cec1ca698fe07cc431fdc
wangyu1 <2391205111@qq.com>
2015-05-21 01:29:48 +0800

    update

--- calcUI/calcTest.js
+++ calcUI/calcTest.js
@@ -4,12 +4,85 @@ QUnit.test("It should handle key press", function( assert ) {
 	$('#3').click();
 	num = calculatorUI.getDisplay() ;
     assert.ok( num == 123 , "Passed!" );
+    $('#AC').click();
 });
-QUnit.test("It should mutiply two numbers", function( assert ) {
-	$('#1').click();
+QUnit.test("It should multiply two numbers", function( assert ) {
+	$('#4').click();
 	$('#multiply').click();
 	$('#3').click();
 	$('#equal').click();
 	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 3 , "Passed!" );
+    assert.ok( num == 12 , "Passed!" );
+    $('#AC').click();
 });
+QUnit.test("It should multiply two numbers", function( assert ) {
+	$('#2').click();
+	$('#dot').click();
+	$('#3').click();
+	$('#multiply').click();
+	$('#1').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num == 2.3 , "Passed!" );
+    $('#AC').click();
+});
+QUnit.test("It should plus two numbers", function( assert ) {
+	$('#1').click();
+	$('#plus').click();
+	$('#1').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num == 2 , "Passed!" );
+     $('#AC').click();
+});
+QUnit.test("It should plus two numbers", function( assert ) {
+	$('#6').click();
+	$('#dot').click();
+	$('#1').click();
+	$('#plus').click();
+	$('#2').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num == 8.1 , "Passed!" );
+    $('#AC').click();
+});
+QUnit.test("It should minus two numbers", function( assert ) {
+	$('#0').click();	
+	$('#minus').click();
+	$('#5').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num == -5 , "Passed!" );
+    $('#AC').click();
+});
+QUnit.test("It should minus two numbers", function( assert ) {
+	$('#5').click();
+	$('#minus').click();
+	$('#4').click();
+	$('#dot').click();
+	$('#5').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num == 0.5 , "Passed!" );
+    $('#AC').click();
+});
+QUnit.test("It should divide two numbers", function( assert ) {
+	$('#5').click();	
+	$('#divide').click();
+	$('#2').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num == 2.5, "Passed!" );
+});
+QUnit.test("It should divide two numbers", function( assert ) {
+	$('#8').click();
+	$('#dot').click();
+	$('#4').click();
+	$('#4').click();
+	$('#divide').click();
+	$('#2').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num == 4.22, "Passed!" );
+});
+
--- calcUI/calculator.js
+++ calcUI/calculator.js
@@ -1,25 +1,75 @@
 var calculator = {
 	memory : 0 ,
 	number : 0 ,
+	count  : 0 ,
+    fuhao  : "",
+	dot    : 0,
 	numberPressed : function(num) {
 		console.log("number pressed: "+num) ;
+		if(this.dot==0){
 		this.number = this.number * 10 + num ;
-		calculatorUI.setDisplay(this.number) ;
+		this.count=this.number;
+			calculatorUI.setDisplay(this.number) ;
+		}
+		if(this.dot==1){
+			hash=$("#display").html()+num;
+			this.number=Number(hash);
+			calculatorUI.setDisplay(this.number) ;
+			
+		
+		}
 	},
 	operatorPressed : function(operator) {
-		alert.log("operator pressed: "+operator) ;	
+		console.log("operator pressed: "+operator) ;
+	
+	this.memory=Number(calculatorUI.getDisplay()) ;
+	this.fuhao=operator;
+	this.number=0;
+	this.dot=0;
+	hashf = "#" +operator 
+		calculatorUI.setDisplay2($(hashf).html()) ;
+		
 	},
 	equalPressed : function() {
 		console.log("equal pressed") ;
+		if(this.fuhao=="plus"){
+			this.count=this.memory+this.number;
+			}
+		if(this.fuhao=="minus"){
+			this.count=this.memory-this.number;
+			}
+		if(this.fuhao=="multiply"){
+			this.count=this.memory*this.number;
+			}
+		if(this.fuhao=="divide"){
+			this.count=this.memory/this.number;
+			}
+			this.number=0;
+			this.dot=0;
+		calculatorUI.setDisplayc(this.count) ;
+		
 	},
 	dotPressed : function() {
 		console.log("dot pressed") ;
+		hashd=calculatorUI.getDisplay()+".";
+		this.dot=1;
+		this.number=0;
+		calculatorUI.setDisplay2(hashd) ;
+		
+		
 	},
 	clearPressed : function() {
 		console.log("clear pressed") ;
+		this.number=0;
+		calculatorUI.setDisplay(this.number);
 	},
 	allClearPressed : function() {
 		console.log("all clear pressed") ;
+		this.count=0;
+		this.memory=0;
+		this.number=0 ,
+	    this.dot=0,
+		calculatorUI.setDisplayc(this.count) ;
 	},
 };
 var calculatorUI = {
@@ -39,8 +89,10 @@ var calculatorUI = {
 		}
 		$("#equal").click(function() {
 			calculator.equalPressed() ;
+
 		});
 		$("#dot").click(function() {
+			
 			calculator.dotPressed() ;
 		});
 		$("#C").click(function() {
@@ -52,6 +104,12 @@ var calculatorUI = {
 	},
 	setDisplay : function() {
 	    $("#display").text(calculator.number) ; 	
+	},
+	setDisplayc : function() {
+	    $("#display").text(calculator.count) ; 	
+	},
+	setDisplay2 : function(a) {
+	    $("#display").html(a) ; 	
 	},
 	getDisplay : function() {
 		return $("#display").text() ;
